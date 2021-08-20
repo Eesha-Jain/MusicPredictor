5608578d4d3230bf31f48064dc23612353e7a834
Eesha-Jain <eesha.jain801@gmail.com>
2021-08-20 14:51:24 -0700

    Finished basic front-end. Will make it look better later

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import '../scss/App.scss';
-import {useState} from 'react';
 
 function App() {
   return (
     <div className="App">
+      <div className="formContainer">
+        <div className="container">
+          <h1>Music Predictor</h1>
+          <form>
+            <label for="age">What is your age?</label><br /><br />
+            <input type="number" min="10" max="130" name="age" placeholder="number" />
+
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
