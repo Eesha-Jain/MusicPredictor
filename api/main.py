eb559acc9a51d5d283d3515d9cd6492bb0fecbc0
Eesha-Jain <eesha.jain801@gmail.com>
2021-08-23 18:06:58 -0700

    Integrated ml

--- api/main.py
+++ api/main.py
@@ -1,3 +1,13 @@
 import sys
-print("Python")
-print("Age: " + sys.argv[1])
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+music_data = pd.read_csv("music.csv")
+X = music_data.drop(columns=['genre'])
+y = music_data['genre']
+
+model = DecisionTreeClassifier()
+model.fit(X, y)
+predictions = model.predict([ [sys.argv[1]] ])
+
+print(predictions)
